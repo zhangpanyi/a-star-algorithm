ceb33ea287643c64b0e8e29a46126c8543894e5b
zhangpanyi <zhangpanyi@live.com>
2015-03-27 09:55:17 +0800

    Update AStar.cpp

--- Classes/AStar.cpp
+++ Classes/AStar.cpp
@@ -14,16 +14,16 @@ bool HeapComp(const AStar::Node *a, const AStar::Node *b)
 AStar::AStar()	: m_row(0)
 	, m_col(0)
 	, m_mapSize(0)
-	, m_nodeMaps(nullptr)
+	, m_mapIndex(nullptr)
 	, m_callBack(nullptr)
 {
 }
 
 AStar::~AStar()
 {
-	if (m_nodeMaps)
+	if (m_mapIndex)
 	{
-		delete[] m_nodeMaps;
+		delete[] m_mapIndex;
 	}
 }
 
@@ -34,50 +34,52 @@ void AStar::init(const AStarDef &def)
 	m_col = def.col;
 	m_callBack = def.canReach;
 
-	if (m_nodeMaps)
+	if (m_mapIndex)
 	{
 		if (m_mapSize < m_row * m_col)
 		{
-			delete[] m_nodeMaps;
+			delete[] m_mapIndex;
 			m_mapSize = m_row * m_col;
-			m_nodeMaps = new NodeState[m_mapSize];
+			m_mapIndex = new NodeState[m_mapSize];
 		}
 	}
 	else
 	{
 		m_mapSize = m_row * m_col;
-		m_nodeMaps = new NodeState[m_mapSize];
+		m_mapIndex = new NodeState[m_mapSize];
 	}
 }
 
 // 清理
 void AStar::clear()
 {
-	for (int i = 0; i < m_row * m_col; ++i)
+	for (int index = 0; index < m_row * m_col; ++index)
 	{
-		if (m_nodeMaps[i].ptr)
+		if (m_mapIndex[index].ptr)
 		{
-			delete m_nodeMaps[i].ptr;
-			m_nodeMaps[i].ptr = nullptr;
+			delete m_mapIndex[index].ptr;
+			m_mapIndex[index].ptr = nullptr;
 		}
+		m_mapIndex[index].state = NOTEXIST;
 	}
 
 	m_row = 0;
 	m_col = 0;
+	m_openList.clear();
 	m_callBack = nullptr;
 }
 
 // 格子是否存在于开启列表
-AStar::Node* AStar::isExistInOpenList(const Grid &grid)
+inline AStar::Node* AStar::isExistInOpenList(const Grid &grid)
 {
-	NodeState &node = m_nodeMaps[grid.row * m_row + grid.col];
-	return node.state == INOPENLIST ? node.ptr : nullptr;
+	NodeState &node = m_mapIndex[grid.row * m_row + grid.col];
+	return node.state == IN_OPENLIST ? node.ptr : nullptr;
 }
 
 // 格子是否存在于关闭列表
-bool AStar::isExistInCloseList(const Grid &point)
+inline bool AStar::isExistInCloseList(const Grid &point)
 {
-	return m_nodeMaps[point.row * m_row + point.col].state == INCLOSELIST;
+	return m_mapIndex[point.row * m_row + point.col].state == IN_CLOSELIST;
 }
 
 // 查询格子是否可通行
@@ -107,10 +109,8 @@ bool AStar::isCanReached(const Grid &current, const Grid &target, bool allowCorn
 	}
 	else if (allowCorner)
 	{
-		int gapX = target.col - current.col;
-		int gapY = target.row - current.row;
-		return (isCanReach(Grid(current.col + gapX, current.row))
-			&& isCanReach(Grid(current.col, current.row + gapY)));
+		return (isCanReach(Grid(current.col + target.col - current.col, current.row))
+			&& isCanReach(Grid(current.col, current.row + target.row - current.row)));
 	}
 
 	return false;
@@ -135,7 +135,7 @@ void AStar::searchCanReached(std::vector<Grid> &around, const Grid &current, boo
 }
 
 // 计算G值
-int AStar::calculG(Node *parent, const Grid &current)
+inline int AStar::calculG(Node *parent, const Grid &current)
 {
 	int value = ((abs(current.col - parent->pos.col) + abs(current.row - parent->pos.row)) == 2 ? OBLIQUE : STEP);
 	value += parent->g;
@@ -143,7 +143,7 @@ int AStar::calculG(Node *parent, const Grid &current)
 }
 
 // 计算H值
-int AStar::calculH(const Grid &current, const Grid &end)
+inline int AStar::calculH(const Grid &current, const Grid &end)
 {
 	int value = abs(end.col - current.col) + abs(end.row - current.row);
 	return value * STEP;
@@ -152,11 +152,11 @@ int AStar::calculH(const Grid &current, const Grid &end)
 // 获取节点在开启列表中的索引值
 int AStar::getIndex(Node *pNode)
 {
-	for (unsigned int i = 0; i < m_openList.size(); ++i)
+	for (unsigned int index = 0; index < m_openList.size(); ++index)
 	{
-		if (m_openList[i]->pos == pNode->pos)
+		if (m_openList[index]->pos == pNode->pos)
 		{
-			return i;
+			return index;
 		}
 	}
 	return -1;
@@ -202,12 +202,11 @@ void AStar::notFoundNode(Node *currentNode, Node *newNode, const Grid &end)
 	newNode->g = calculG(currentNode, newNode->pos);
 	newNode->h = calculH(newNode->pos, end);
 
-	m_openList.push_back(newNode);
-
-	NodeState &node = m_nodeMaps[newNode->pos.row * m_row + newNode->pos.col];
+	NodeState &node = m_mapIndex[newNode->pos.row * m_row + newNode->pos.col];
 	node.ptr = newNode;
-	node.state = INOPENLIST;
+	node.state = IN_OPENLIST;
 
+	m_openList.push_back(newNode);
 	std::push_heap(m_openList.begin(), m_openList.end(), HeapComp);
 }
 
@@ -227,26 +226,27 @@ inline bool AStar::validAStarDef(const AStarDef &def)
 	return false;
 }
 
-// 执行A*算法
-std::deque<Grid> AStar::operator() (const AStarDef &def)
+// 执行A*搜索
+std::deque<Grid> AStar::search(const AStarDef &def)
 {
 	std::deque<Grid> searchPath;
 	if (validAStarDef(def))
 	{
+		// 初始化
 		init(def);
 
 		// 周围可通行格
 		std::vector<Grid> around;
-		around.reserve(8);
+		around.reserve(def.allowCorner ? 8 : 4);
 
 		// 将起点放入开启列表
 		Node *start = new Node(def.start);
 		m_openList.push_back(start);
 
-		// 更新节点地图
-		NodeState &node = m_nodeMaps[start->pos.row * m_row + start->pos.col];
+		// 更新地图索引
+		NodeState &node = m_mapIndex[start->pos.row * m_row + start->pos.col];
 		node.ptr = start;
-		node.state = INOPENLIST;
+		node.state = IN_OPENLIST;
 
 		while (!m_openList.empty())
 		{
@@ -256,38 +256,39 @@ std::deque<Grid> AStar::operator() (const AStarDef &def)
 			m_openList.pop_back();
 
 			// 放入关闭列表
-			m_nodeMaps[currentNode->pos.row * m_row + currentNode->pos.col].state = INCLOSELIST;
+			m_mapIndex[currentNode->pos.row * m_row + currentNode->pos.col].state = IN_CLOSELIST;
 
 			// 搜索可通行格子
 			around.clear();
 			searchCanReached(around, currentNode->pos, def.allowCorner);
 
 			// 遍历可通行格子
-			for (auto itr = around.begin(); itr != around.end(); ++itr)
+			unsigned int size = around.size();
+			for (unsigned int index = 0; index < size; ++index)
 			{
-				Node *newNode = isExistInOpenList(*itr);
+				Node *newNode = isExistInOpenList(around[index]);
 				if (newNode)
 				{
 					foundNode(currentNode, newNode);
 				}
 				else
 				{
-					newNode = new Node(*itr);
+					newNode = new Node(around[index]);
 					notFoundNode(currentNode, newNode, def.end);
 
-					if ((*itr) == def.end)
+					if (around[index] == def.end)
 					{
 						while (newNode->parent)
 						{
 							searchPath.push_front(newNode->pos);
 							newNode = newNode->parent;
 						}
-						m_openList.clear();
-						break;
+						goto EndSearch;
 					}
 				}
 			}
 		}
+EndSearch:
 		clear();
 	}
 	else
@@ -295,5 +296,6 @@ std::deque<Grid> AStar::operator() (const AStarDef &def)
 		std::cerr << "Invalid AStarDef!" << std::endl;
 		assert(false);
 	}
+
 	return searchPath;
 }
