1a13ec2f89a16dc42711b2caf85bbc7ef7db0f13
zhangpanyi <zhangpanyi@live.com>
2015-04-24 17:43:17 +0800

    update

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	char astar_map[1000][1000] =
+	char maps[1000][1000] =
 	{
 		{ 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
 		{ 0, 0, 0, 1, 0, 1, 0, 1, 0, 1 },
@@ -19,15 +19,13 @@ int main()
 		{ 0, 0, 0, 0, 0, 0, 1, 0, 1, 0 },
 	};
 
-	auto CanReach = [&](const Point &grid)->bool
-	{
-		return astar_map[grid.row][grid.col] == 0;
-	};
-
 	AStarDef def;
 	def.row = 1000;
 	def.col = 1000;
-	def.can_reach = CanReach;
+	def.can_reach = [&](const Point &grid)->bool
+	{
+		return maps[grid.row][grid.col] == 0;
+	};
 	def.start_point = Point(0, 0);
 	def.end_point = Point(999, 999);
 	def.allow_corner = false;
