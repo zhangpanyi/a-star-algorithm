17a5df781529237e02a7608e5d867950e3c9dc22
zhangpanyi@live.com <zhangpanyi@live.com>
2016-03-20 18:13:38 +0800

    update

--- test/Test.cpp
+++ test/Test.cpp
@@ -1,6 +1,42 @@
-﻿#include <iostream>
-#include "Duration.h"
-#include "src/AStar.h"
+﻿#include <chrono>
+#include <iostream>
+#include "AStar.h"
+
+class Duration
+{
+public:
+	Duration()
+		: start_time_(std::chrono::system_clock::now())
+	{}
+
+	void reset()
+	{
+		start_time_ = std::chrono::system_clock::now();
+	}
+
+	std::chrono::seconds::rep seconds()
+	{
+		return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - start_time_).count();
+	}
+
+	std::chrono::milliseconds::rep milli_seconds()
+	{
+		return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start_time_).count();
+	}
+
+	std::chrono::microseconds::rep micro_seconds()
+	{
+		return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - start_time_).count();
+	}
+
+	std::chrono::nanoseconds::rep nano_seconds()
+	{
+		return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now() - start_time_).count();
+	}
+
+private:
+	std::chrono::system_clock::time_point start_time_;
+};
 
 int main(int argc, char *argv[])
 {
