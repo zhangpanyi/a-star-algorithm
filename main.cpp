73fa8a793b0e8e2ca60e49f521a78a335265921f
zhangpanyi <zhangpanyi@live.com>
2016-10-09 17:49:07 +0800

    update

---
+++ main.cpp
@@ -0,0 +1,47 @@
+﻿#include <chrono>
+#include <iostream>
+#include "src/a-star.h"
+#include "src/blockallocator.h"
+
+int main(int argc, char *argv[])
+{
+	char maps[1000][1000] =
+	{
+		{ 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
+		{ 0, 0, 0, 1, 0, 1, 0, 1, 0, 1 },
+		{ 1, 1, 1, 1, 0, 1, 0, 1, 0, 1 },
+		{ 0, 0, 0, 1, 0, 0, 0, 1, 0, 1 },
+		{ 0, 1, 0, 1, 1, 1, 1, 1, 0, 1 },
+		{ 0, 1, 0, 0, 0, 0, 0, 0, 0, 1 },
+		{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 0, 0, 0, 0, 1, 0, 0, 0, 1, 0 },
+		{ 1, 1, 0, 0, 1, 0, 1, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 1, 0, 1, 0 },
+	};
+
+	// 搜索参数
+	AStar::Param param;
+	param.width = 1000;
+	param.height = 1000;
+	param.corner = false;
+	param.start = AStar::Vec2(0, 0);
+	param.end = AStar::Vec2(999, 999);
+	param.can_reach = [&](const AStar::Vec2 &pos)->bool
+	{
+		return maps[pos.y][pos.x] == 0;
+	};
+
+	// 执行搜索
+    BlockAllocator allocator;
+	AStar algorithm(&allocator);
+
+    auto start_time = std::chrono::system_clock::now();
+	auto path = algorithm.find(param);
+    auto end_time = std::chrono::system_clock::now();
+
+	std::cout << (path.empty() ? "路径未找到！" : "路径已找到！") << std::endl;
+    std::cout << "本次寻路耗时" << std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count() << "纳秒" << std::endl;
+
+    system("pause");
+	return 0;
+}
